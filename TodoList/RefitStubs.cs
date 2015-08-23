2b2e30f46657d7cb2d13b3aeba8b3955957924f3
Aleksander Piotrowski <pelotasplus@gmail.com>
2015-08-23 19:40:15 +0200

    TodoList: fix Refit

---
+++ TodoList/RefitStubs.cs
@@ -0,0 +1,5 @@
+﻿/* ******** Hey You! *********
+ *
+ * This is a generated file, and gets rewritten every time you build the
+ * project. If you want to edit it, you need to edit the mustache template
+ * in the Refit package */
